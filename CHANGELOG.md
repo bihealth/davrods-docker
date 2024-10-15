8c2a969f8c7fe84691e752964790ed0e2ce9dd0b
Mikko Nieminen <mikko.nieminen@bih-charite.de>
2024-10-15 17:01:51 +0200

    update versioning, add changelog (#5)

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+
+## Unreleased
+
+- Stop building `latest` version of image (#5)
+- Add build version to `build.sh` (#5)
+- Add changelog (#5)
+
+## 4.2.11_1.5.0-1 (2024-10-15)
+
+- Tag release for legacy Davrods v4.2.11 image
--- build.sh
+++ build.sh
@@ -2,13 +2,12 @@
 
 export REPO=ghcr.io/bihealth/davrods-docker
 export DAVRODS_PKG_VERSION=${DAVRODS_PKG_VERSION-4.2.11_1.5.0}
+export BUILD_VERSION=${BUILD_VERSION-1}
 
 docker build \
-    -t "${REPO}:${DAVRODS_PKG_VERSION}" \
-    -t "${REPO}:latest" \
+    -t "${REPO}:${DAVRODS_PKG_VERSION}-${BUILD_VERSION}" \
     --target main \
     docker
 
 echo "Now do:"
-echo "docker push ${REPO}:${DAVRODS_PKG_VERSION}"
-echo "docker push ${REPO}:latest"
+echo "docker push ${REPO}:${DAVRODS_PKG_VERSION}-${BUILD_VERSION}"
